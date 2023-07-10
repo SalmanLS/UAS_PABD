1054120fb64c9a08916867e0ecd42165f441db04
DimasLuxMundi <dimasluxmundi@gmail.com>
2023-07-10 17:07:48 +0700

    displayed reservasi table on daftar reservasi

--- UAS_PABD/Daftar_Reservasi.Designer.cs
+++ UAS_PABD/Daftar_Reservasi.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,7 +39,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(247, 27);
+            this.label1.Location = new System.Drawing.Point(258, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(294, 32);
             this.label1.TabIndex = 0;
@@ -58,23 +59,36 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(115, 79);
+            this.dataGridView1.Location = new System.Drawing.Point(104, 79);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(579, 308);
             this.dataGridView1.TabIndex = 2;
             // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(368, 411);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(81, 27);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
             // Daftar_Reservasi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Daftar_Reservasi";
             this.Text = "Daftar_Reservasi";
+            this.Load += new System.EventHandler(this.Daftar_Reservasi_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,5 +100,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
--- UAS_PABD/Daftar_Reservasi.cs
+++ UAS_PABD/Daftar_Reservasi.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,24 @@ namespace UAS_PABD
 {
     public partial class Daftar_Reservasi : Form
     {
+        private string stringConnection = "data source=DESKTOP-NKUDL8D\\DIMASDAMAR;" +
+            "database=Villa;User ID=sa; Password=magic118";
+        private SqlConnection koneksi;
         public Daftar_Reservasi()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+
+        }
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select Reservasi.kode_reservasi,Tamu.id_tamu, Tamu.namaTamu , Reservasi.tgl_reservasi, Reservasi.lamaSewa, Reservasi.no_villa\r\nfrom Reservasi\r\ninner join Tamu on Reservasi.id_tamu = Tamu.id_tamu;";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +38,17 @@ namespace UAS_PABD
             Daftar_Data dd = new Daftar_Data();
             dd.Show();
             this.Hide();
+        }
+
+        private void Daftar_Reservasi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            btnOpen.Enabled = false;
         }
     }
 }
