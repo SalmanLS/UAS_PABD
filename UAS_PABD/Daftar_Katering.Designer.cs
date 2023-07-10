380151d60a53b9cac1bb52e617fa4111093f1bd6
DimasLuxMundi <dimasluxmundi@gmail.com>
2023-07-10 21:32:50 +0700

    add katering table for daftar katering

--- UAS_PABD/Daftar_Katering.Designer.cs
+++ UAS_PABD/Daftar_Katering.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,16 +66,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(370, 386);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
             // Daftar_Katering
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Daftar_Katering";
             this.Text = "Daftar_Katering";
+            this.Load += new System.EventHandler(this.Daftar_Katering_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,5 +99,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
--- UAS_PABD/Daftar_Katering.cs
+++ UAS_PABD/Daftar_Katering.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,41 @@ namespace UAS_PABD
 {
     public partial class Daftar_Katering : Form
     {
+        private string stringConnection = "data source=DESKTOP-NKUDL8D\\DIMASDAMAR;" +
+            "database=Villa;User ID=sa; Password=magic118";
+        private SqlConnection koneksi;
         public Daftar_Katering()
         {
             InitializeComponent();
-        }
+            koneksi = new SqlConnection(stringConnection);
 
+        }
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select Katering.id_katering, Jenis_Katering.jenisKatering, Tamu.namaTamu, Reservasi.no_villa , Pemesanan.porsi, Pemesanan.totalHarga,Katering.hari, Pemesanan.tgl_pemesanan\r\nfrom Katering\r\ninner join Jenis_Katering on Katering.kode_jenisKatering = Jenis_Katering.kode_jenisKatering\r\ninner join Pemesanan on Katering.id_katering = Pemesanan.id_katering\r\ninner join Tamu on Tamu.id_tamu = Pemesanan.id_tamu\r\ninner join Reservasi on Reservasi.id_tamu = Pemesanan.id_tamu;";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Daftar_Data dd = new Daftar_Data();
             dd.Show();
             this.Hide();
+        }
+
+        private void Daftar_Katering_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            btnOpen.Enabled = false;
         }
     }
 }
